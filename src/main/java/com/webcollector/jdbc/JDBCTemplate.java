320c4b5648190a5a85b2df06cda9543a8c148209
liyongqiang1210 <1240965061@qq.com>
2017-08-24 21:49:07 +0800

    添加SpringJDBC支持

---
+++ src/main/java/com/webcollector/jdbc/JDBCTemplate.java
@@ -0,0 +1,34 @@
+package com.webcollector.jdbc;
+
+import java.util.UUID;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.webcollector.util.DateUtil;
+
+public class JDBCTemplate {
+
+	private Log log = LogFactory.getLog(JDBCTemplate.class);
+	public void jdbc(String type,String title, String web_url, String release_time, String source, String text,String sql) {
+		JdbcTemplate jdbcTemplate = null;
+		try {
+			jdbcTemplate = JDBCHelper.createMysqlTemplate("maven",
+					"jdbc:mysql://localhost:3306/maven", "root", "root",
+					0, 20);
+			if (jdbcTemplate != null) {
+				int updates = jdbcTemplate.update(
+						sql,
+						UUID.randomUUID().toString(), type, title, text, source, web_url, release_time,new DateUtil().getToday()
+						);
+				if (updates == 1) {
+					log.debug(title+"  插入成功");
+				}
+			}
+		} catch (Exception ex) {
+			jdbcTemplate = null;
+			System.out.println("mysql未开启或JDBCHelper.createMysqlTemplate中参数配置不正确!");
+		}
+	}
+}
