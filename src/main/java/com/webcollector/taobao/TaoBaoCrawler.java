d415e4e60262f8242276558e39fb01a96a1ad34a
liyongqiang1210 <1240965061@qq.com>
2017-09-11 21:39:46 +0800

    ..

---
+++ src/main/java/com/webcollector/taobao/TaoBaoCrawler.java
@@ -0,0 +1,12 @@
+package com.webcollector.taobao;
+
+import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
+
+public abstract class TaoBaoCrawler extends BreadthCrawler{
+
+	public TaoBaoCrawler(String crawlPath, boolean autoParse) {
+		super(crawlPath, autoParse);
+		// TODO Auto-generated constructor stub
+	}
+
+}
