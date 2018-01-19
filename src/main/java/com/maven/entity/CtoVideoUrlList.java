542cdd5baf34628f939de26af298a02904c08479
liyongqiang <liyongqiang@DESKTOP-E4NHH8S>
2018-01-19 17:10:53 +0800

    添加51cto视频课程的实体类

---
+++ src/main/java/com/maven/entity/CtoVideoUrlList.java
@@ -0,0 +1,68 @@
+package com.maven.entity;
+
+public class CtoVideoUrlList {
+
+	private Integer video_id;
+	private String video_url;
+	private String video_title;
+	private String video_price;
+	private String video_number;
+	private String video_class_hour;
+	private Integer video_is_craw;
+
+	public Integer getVideo_id() {
+		return video_id;
+	}
+
+	public void setVideo_id(Integer video_id) {
+		this.video_id = video_id;
+	}
+
+	public String getVideo_url() {
+		return video_url;
+	}
+
+	public void setVideo_url(String video_url) {
+		this.video_url = video_url;
+	}
+
+	public String getVideo_title() {
+		return video_title;
+	}
+
+	public void setVideo_title(String video_title) {
+		this.video_title = video_title;
+	}
+
+	public String getVideo_price() {
+		return video_price;
+	}
+
+	public void setVideo_price(String video_price) {
+		this.video_price = video_price;
+	}
+
+	public String getVideo_number() {
+		return video_number;
+	}
+
+	public void setVideo_number(String video_number) {
+		this.video_number = video_number;
+	}
+
+	public String getVideo_class_hour() {
+		return video_class_hour;
+	}
+
+	public void setVideo_class_hour(String video_class_hour) {
+		this.video_class_hour = video_class_hour;
+	}
+
+	public Integer getVideo_is_craw() {
+		return video_is_craw;
+	}
+
+	public void setVideo_is_craw(Integer video_is_craw) {
+		this.video_is_craw = video_is_craw;
+	}
+}
