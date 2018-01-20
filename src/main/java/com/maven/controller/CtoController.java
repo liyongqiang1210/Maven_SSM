c1428268471e0e1c86eb37b5e9999ce2db42d4f2
liyongqiang <liyongqiang@DESKTOP-E4NHH8S>
2018-01-20 16:30:12 +0800

    修改selectCourseByVideoID方法的测试参数

--- src/main/java/com/maven/controller/CtoController.java
+++ src/main/java/com/maven/controller/CtoController.java
@@ -19,17 +19,28 @@ public class CtoController {
 	@Autowired
 	private CTOService ctoService;
 
+	/**
+	 * 获取详细课程方法
+	 * 
+	 * @param video_url_id
+	 * @return
+	 */
 	@RequestMapping(value = "/selectCourseByVideoID", produces = "text/html;charset=UTF-8;")
 	@ResponseBody
 	public String selectCourseByVideoID(Integer video_url_id) {
 
-		List<Map<String, String>> CourseList = ctoService.selectCourseByVideoID(4186);
+		List<Map<String, String>> CourseList = ctoService.selectCourseByVideoID(video_url_id);
 		JSONArray json = JSONArray.fromObject(CourseList);
-		
+
 		return json.toString();
 
 	}
 
+	/**
+	 * 获取视频链接方法
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/selectVideoUrlList", produces = "text/html;charset=UTF-8;")
 	@ResponseBody
 	public String selectVideoUrlList() {
